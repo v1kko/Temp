7b9fae418566d7c33deab7f57f237ae2f5916541
Cedric Axel Blom <cedric@Dell.(none)>
2012-06-05 16:19:21 +0200

    nog afmaken

--- Modules/steering.py
+++ Modules/steering.py
@@ -1,16 +1,27 @@
+import control
+
+
 class Steering:
     def __init__(self, step_dist, step_angle):
-        self.__step_dist = step_dist
-        self.__step_angle = step_angle
+        self.step_dist = step_dist
+        self.step_angle = step_angle
+        self.__
     
-    def add(self, type, step_size):
-        if (type == 'MOVE'):
+    def add(self, set_type, step_size):
+        if set_type == 'MOVE':
             self.__step_dist = step_size
-        else:
+        elif set_type == 'TURN':
             self.__step_angle = step_size
     
     def move(self, speed, distance):
+        
         pass
     
     def turn(self, speed, angle):
         pass
+    
+    def __odometry(self):
+        if not control.send('SENSORS GET ODOMETRY'):
+            return None
+        module, data = control.receive(True).split(' ', 1)
+            
