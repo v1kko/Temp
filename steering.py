a3b42c2304cb866ef31f159ce02b39b906305da2
Thomas <dutchling@gmail.com>
2012-06-05 15:55:09 +0200

    tunnelling

--- steering.py
+++ steering.py
@@ -2,11 +2,11 @@ class Steering:
     def __init__(self):
         pass
     
-    def add(self):
+    def add(self, type, step_size):
         pass
     
-    def move(self):
+    def move(self, speed, distance):
         pass
     
-    def turn(self):
+    def turn(self, speed, angle):
         pass
