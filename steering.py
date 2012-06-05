d67f1a927f0568b22af2857b7af97d7e03d448f2
Cedric Axel Blom <cedric@Dell.(none)>
2012-06-05 14:49:33 +0200

    temp

--- steering.py
+++ steering.py
@@ -1,12 +1,16 @@
 class Steering:
-    def __init__(self):
+    def __init__(self, step_dist, step_angle):
+        self.__step_dist = step_dist
+        self.__step_angle = step_angle
+    
+    def add(self, type, step_size):
+        if (type == 'MOVE'):
+            self.__step_dist = step_size
+        else:
+            self.__step_angle = step_size
+    
+    def move(self, speed, distance):
         pass
     
-    def add(self):
-        pass
-    
-    def move(self):
-        pass
-    
-    def turn(self):
+    def turn(self, speed, angle):
         pass
