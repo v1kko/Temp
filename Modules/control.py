258183694fb90bb32e0abd7c325924d9b935f502
Vikko <veterazizi@hotmail.com>
2012-06-06 00:03:25 +0200

    Config.py added + update control.py

--- Modules/control.py
+++ Modules/control.py
@@ -1,6 +1,19 @@
 def receive (no_block=False):
+	"""
+	This is the receive function implemented in every Module to communicate
+	with other modules
+	Returns the source and data
+	Returns None when no_block = true but no data is received
+	"""
 	src = "STUB"
 	data = "STUB"
 	return (src, data)
 def send (dest, data):
+	"""
+	This is the send function implemented in every module to communicate
+	with other modules
+	Needs the destination and data to send
+	Returns False on error
+	Returns True on succes
+	"""
 	return
---
+++ config.py
@@ -0,0 +1,8 @@
+modules = {
+#Format = Modulename : (port, host, login, password)
+#Example:
+	#'module':(1000, 'deze', 'vikko', 'vokko')
+	'sensors':(4325, 'localhost'),
+	'steering':(5432, 'localhost'),
+	'interface':(7664, 'localhost')
+}
