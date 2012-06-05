d56e2984cf041ba3faea174c6b09ce88e0c63fa8
Cedric Axel Blom <cedric@Dell.(none)>
2012-06-05 23:39:00 +0200

    odometry function done

--- Modules/steering.py
+++ Modules/steering.py
@@ -1,26 +1,54 @@
+from Queue import Queue
 import control
 
 
 class Steering:
-    def __init__(self, step_dist, step_angle):
-        self.step_dist = step_dist
-        self.step_angle = step_angle
-        self.__
+    def __init__(self, grid_size, angle_size):
+        self.grid_size = grid_size
+        self.angle_size = angle_size
+        self.__command_queue = Queue()
+        self.__active_task = ''
+    
     
     def add(self, set_type, step_size):
-        if set_type == 'MOVE':
-            self.__step_dist = step_size
-        elif set_type == 'TURN':
-            self.__step_angle = step_size
+        if set_type.upper() == 'MOVE':
+            self.step_dist = step_size
+        elif set_type.upper() == 'TURN':
+            self.step_angle = step_size
     
-    def move(self, speed, distance):
-        
+    
+    def move(self, speed, distance):    
         pass
+    
     
     def turn(self, speed, angle):
         pass
     
+    
+    def flush(self):
+        while not self.__command_queue.empty():
+            self.__command_queue.get()
+    
+    
     def __odometry(self):
         if not control.send('SENSORS GET ODOMETRY'):
-            return None
-        module, data = control.receive(True).split(' ', 1)
+            return
+        
+        while True:
+            recv = control.receive(True)
+            if not recv or recv.find(' ') == -1:
+                control.send('CONTROL FAIL Expecting message from sensors')
+                return
+            
+            module, data = recv.upper().split(None, 1)
+            if data == 'ALARM':
+                self.flush()
+                return
+            if module == 'SENSORS':
+                return data.split(None)
+            else:
+                if self.__command_queue.full() and \
+                not control.send(self.__active_task + ' FAIL Queue is full'):
+                    return
+                else:
+                    self.__command_queue.put(data)
