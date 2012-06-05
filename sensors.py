c02ab09686cf1a8eea3e99af7e3cf695239e7bdc
Arjen <myself@arjenonline.eu>
2012-06-05 16:03:40 +0200

    4th draft sensors

--- sensors.py
+++ sensors.py
@@ -9,39 +9,43 @@ from control import *
 
 class Sensors():
     def __init__(self):
-        self.sock = None   
-        
         self.data = ""
         
-        self.laser = ""
-        self.odometer = ""
-        self.sonar = ""
-        
-        self.setup(("localhost", 3000))        
+        self.sensors = {"ODOMETRY" : "",
+                        "LASER" : "",
+                        "SONAR" : ""}     
         
         self.running = True
         self.run()
-    
-    def setup(self, addr):
-        self.sock = socket.create_connection(addr)
         
     def run(self):
         while(self.running):
             src, rcv = receive(True)
             
-            self.data = self.data + recv            
-            
-            if data != None:
-                data = data.split('\r\n')
+            if src == "Interface":
+                self.data = self.data + recv
+
+                self.data = self.data.split('\r\n')
 
                 for i in range(len(data) - 1):
                     if not data[i].find("Laser"):
-                        self.laser = data[i]
-                    elif not data[i].find("Odometer"):
-                        self.odometer = data[i]
+                        self.sensors["LASER"] = data[i]
+                        break
+                    elif not data[i].find("Odometry"):
+                        self.sensors["ODOMETRY"] = data[i]
                         break
                     elif not data[i].find("Sonar"):
-                        self.sonar = sonar[i]
+                        self.sensors["SONAR"] = sonar[i]
                         
                 self.data = self.data[len(data) - 1]
-    
+            
+            else:
+                self.data = self.data.split(' ')
+                if self.data[0] == GET:
+                    try:
+                        send(src + ' SENS ' + data[1] + ' ' + self.sensors[data[1]] )
+                    except:
+                        send(src + ' SENS FAIL ' + data[1] + ' ' )
+                        
+                        
+"SENS ODOMETRY <float x> <float y> <float z>"
