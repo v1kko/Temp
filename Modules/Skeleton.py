aec03b7d6cc40dcc74f22e3775ca1ef1b6ad3cb9
Vikko <veterazizi@hotmail.com>
2012-06-07 11:52:24 +0200

    Added Skeleton module

---
+++ Modules/Skeleton.py
@@ -0,0 +1,15 @@
+"""
+This is a skeleton for a Module in our system
+Please remove these lines if you use it
+"""
+from Control import *
+
+class Skeleton:
+    def __init__(self):
+        ctr = Control(self.__class__.__name__)
+        mainloop()
+        exit(0)
+
+    def mainloop:
+        while True
+            print "STUB"
